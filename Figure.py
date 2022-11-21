71c1f0da6600ecd405e7c14e9aa4ac0314cbe9c0
dmitrij.sudnitsyn <dmitrij.sudnitsyn@gmail.com>
2022-11-21 11:58:15 +0200

    Итоговй вариант Прямоугольника

--- Figure.py
+++ Figure.py
@@ -1,6 +1,7 @@
 class Figure:
  def __init__(self,name):
     self.name=name
+    self.guare=0
 
  def getName(self):
     return self.name   
@@ -9,7 +10,7 @@ class Figure:
     self.name=name
 
  def getQuare(self):
-    return 0   
+    return self.guare
 
  def __str__(self) -> str:
     return f"Фигура {self.name}; c площадью: {self.getQuare()}"       
@@ -17,10 +18,22 @@ class Figure:
 class RegTangle(Figure):
   def __init__(self, name,a,b):
     self.a,self.b=a,b
+    self.guare=a*b
     super().__init__(name)
+
+#  Переопределяем вывод данных по прямоугольнику   
+  def __str__(self) -> str:
+    return f"Фигура {self.name};  со сторонами {self.getStoronA()}Х{self.getStoronA()} имеет площадь площадью: {self.getQuare()}"       
+
+  def __int__(self):
+   try:
+    return int(self.guare)
+   except ValueError:
+    return print(f"Значение {self.guare} не численное")
 
   def set(self, name,a,b):
     self.a,self.b=a,b
+    self.guare=a*b
     super().setName(name)
     
 
@@ -32,12 +45,14 @@ class RegTangle(Figure):
 
   def setStoronA(self,a):
     self.a=a
+    super().guare=a*self.b
 
   def getStoronB(self,b):
     self.a=b
+    super().guare=self.a*b
 
   def getQuare(self):  
-     return self.a*self.b
+     return self.guare
 
 
   
